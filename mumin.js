bed912c11e1741f58069fb5a7bb2c3ce2c22e1d9
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-06-17 22:28:15 +0530

    first

---
+++ mumin.js
@@ -0,0 +1,34 @@
+// let a=20;
+// let b=30;
+// if(a>b){
+//     console.log(a)
+// }else{
+//     console.log(b)
+// }
+
+// let a=process.argv[2];
+// let b=process.argv[3]
+
+// console.log(a+b)
+
+// let a=parseInt(process.argv[2]);
+// let b=parseInt(process.argv[3])
+
+// console.log(a+b)
+
+
+// let a=process.argv[2];
+// let b=process.argv[3]
+
+// console.log(process.argv)
+
+
+//Q. Using a loop print all the natural numbers up to and including n
+
+
+// let n=parseInt(process.argv[2])
+// for(let i=1; i<=n;i++){
+//     console.log(i)
+// }
+
+
