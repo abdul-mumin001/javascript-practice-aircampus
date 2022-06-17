831bcc7427306a17059873610b017b1819174c4b
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-06-17 22:41:52 +0530

    second

--- mumin.js
+++ mumin.js
@@ -31,4 +31,12 @@
 //     console.log(i)
 // }
 
+//Q. using a loop print all even numbers up to and including n.
+
+let n=parseInt(process.argv[2])
+for(let i=1; i<=n;i++){
+    if(i%2==0){
+        console.log(i)
+    }
+}
 
