3f5291af8ebf9e3b41244d56c6d60a89b8e3254f
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-03-04 23:38:27 +0530

    js practice

--- mumin.js
+++ mumin.js
@@ -33,10 +33,19 @@
 
 //Q. using a loop print all even numbers up to and including n.
 
-let n=parseInt(process.argv[2])
-for(let i=1; i<=n;i++){
-    if(i%2==0){
-        console.log(i)
-    }
+// let n=parseInt(process.argv[2])
+// for(let i=1; i<=n;i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+
+//Q. print all the numbers from a to b in their reverse order
+
+let a=parseInt(process.argv[2])
+let b=parseInt(process.argv[3])
+for(let i=a; i<=b;i--){
+    console.log(i)
 }
 
